418a1e62ffc7a0779bdad4ba35b9715b35c61def
admin <admin@admins-MacBook-Pro.local>
2021-11-25 14:35:59 +0530

    changed module source

--- eks_vpc/main.tf
+++ eks_vpc/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source                  = "../modules/eks-vpc"
+  source  = "app.terraform.io/shah-demo/general/aws//modules/eks-vpc"
+  version = "1.0.0"
   cluster_name            = var.cluster_name
   eks_vpc_cidr_block      = var.vpc_cidr_block
   eks_public_subnets      = var.public_subnets
